005d71ffdaa51991cab8578af7695259e97fece9
Peter Širka <petersirka@gmail.com>
2016-09-06 08:35:02 +0200

    First commit.

---
+++ definitions/merge.js
@@ -0,0 +1,8 @@
+// Templates
+F.localize('/templates/*.html', ['compress']);
+
+// Styles
+F.merge('/css/default.css', '/css/bootstrap.min.css', '/css/ui.css', '/css/default.css');
+
+// Scripts
+F.merge('/js/default.js', '/js/jctajr.min.js', '/js/ui.js', '/js/default.js');
---
+++ readme.md
@@ -0,0 +1,7 @@
+[![Support](https://www.totaljs.com/img/button-support.png)](https://www.totaljs.com/support/)
+
+# Total.js: Empty Project
+
+- Install Total.js via NPM `$ npm install total.js`
+- Download example
+- Run it `$ node debug.js`
