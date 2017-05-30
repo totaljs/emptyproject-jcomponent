5ee6f9afe89cac28bb8de1c8bcc6c35cb9b0cb4e
Peter Širka <petersirka@gmail.com>
2017-05-30 08:13:10 +0200

    Update example.

--- definitions/merge.js
+++ definitions/merge.js
@@ -2,7 +2,7 @@
 F.localize('/templates/*.html', ['compress']);
 
 // Styles
-F.merge('/css/default.css', '/css/bootstrap.min.css', '/css/ui.css', '/css/default.css');
+F.merge('/css/default.css', '/css/ui.css', '/css/default.css');
 
 // Scripts
-F.merge('/js/default.js', '/js/jctajr.min.js', '/js/ui.js', '/js/default.js');
+F.merge('/js/default.js', '/js/ui.js', '/js/default.js');
